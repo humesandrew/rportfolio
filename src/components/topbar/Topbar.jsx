017fe2fda090cfdc1b5360ec32de41abc6688bb3
Andy Humes <andrew.humes@du.edu>
2022-04-07 17:28:03 -0600

    page layout

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,22 @@
+import "./topbar.scss";
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">My logo</a>
+                    <div className="itemContainer">
+                        
+                    </div>
+                    
+                </div>
+             
+                <div className="right">
+                This is right. 
+                </div>
+
+            </div>
+        </div>
+    )
+}
