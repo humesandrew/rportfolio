b4653244a4598acae71ee1073b814b1ea8fd7999
Andy Humes <andrew.humes@du.edu>
2022-04-07 23:36:57 -0600

    hamburgertime

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,22 +1,36 @@
 import "./topbar.scss";
+import AccessibilityNewIcon from "@mui/icons-material/AccessibilityNew";
 
 export default function Topbar() {
-    return (
-        <div className="topbar">
-            <div className="wrapper">
-                <div className="left">
-                    <a href="#intro" className="logo">My logo</a>
-                    <div className="itemContainer">
-                        
-                    </div>
-                    
-                </div>
-             
-                <div className="right">
-                This is right. 
-                </div>
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            My logo
+          </a>
+          <div className="itemContainer">
+            <AccessibilityNewIcon className="icon" />
+            <span>614-YOUR-MOM</span>
+          </div>
 
-            </div>
+          <div className="itemContainer">
+            <AccessibilityNewIcon className="icon" />
+            <span>yourmom@yourmom.com</span>
+          </div>
         </div>
-    )
+
+        <div className="right">
+            <div className="hamburger">
+                <span className="line1"></span>
+                <span className="line2"></span>
+                <span className="line3"></span>
+            </div>
+
+
+
+        </div>
+      </div>
+    </div>
+  );
 }
