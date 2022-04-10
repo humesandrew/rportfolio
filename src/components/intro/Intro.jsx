2b5be691ba4b546cca1801a423b56918e64ee0af
Andy Humes <andrew.humes@du.edu>
2022-04-10 15:53:12 -0600

    contact page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,31 @@
 import "./intro.scss";
 
+import { useEffect, useRef } from "react";
+
 export default function Intro() {
-    return (
-        <div className="intro" id="intro">
-           
+  const textRef = useRef();
+
+
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+       
+          <h1>Andy Humes</h1>
+          <h3>
+            Junior Developer <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            Andy Humes
+            Portfolio 
           </a>
           <div className="itemContainer">
             <Person className="icon" />
