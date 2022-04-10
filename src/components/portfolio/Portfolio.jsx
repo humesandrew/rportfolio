e3ba6a25e0cc18aafbb7644bc86e26e501f87444
Andy Humes <andrew.humes@du.edu>
2022-04-10 16:38:56 -0600

    adding portfolio cards

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,81 @@
 import "./portfolio.scss";
 
 export default function Portfolio() {
-    return (
-        <div className="portfolio" id="portfolio">
-           
-        </div>
-    )
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <div className="container">
+        <ul>
+          <div className="item">
+            <li>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFBVgrCZUwUT9V-rLSpQPj10C8reI2lUodOA&usqp=CAU"
+                alt=""
+              />
+              <h3>ChartMD</h3>
+            </li>
+          </div>
+
+          <div className="item">
+            <li>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFBVgrCZUwUT9V-rLSpQPj10C8reI2lUodOA&usqp=CAU"
+                alt=""
+              />
+              <h3>Belly-Buster</h3>
+            </li>
+          </div>
+
+          <div className="item">
+            <li>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFBVgrCZUwUT9V-rLSpQPj10C8reI2lUodOA&usqp=CAU"
+                alt=""
+              />
+              <h3>Portfolio: JavaScript</h3>
+            </li>
+          </div>
+
+          <div className="item">
+            <li>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFBVgrCZUwUT9V-rLSpQPj10C8reI2lUodOA&usqp=CAU"
+                alt=""
+              />
+              <h3>Employee Directory</h3>
+            </li>
+          </div>
+
+          <div className="item">
+            <li>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFBVgrCZUwUT9V-rLSpQPj10C8reI2lUodOA&usqp=CAU"
+                alt=""
+              />
+              <h3>Schedule App</h3>
+            </li>
+          </div>
+
+          <div className="item">
+            <li>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFBVgrCZUwUT9V-rLSpQPj10C8reI2lUodOA&usqp=CAU"
+                alt=""
+              />
+              <h3>ReadmeGen</h3>
+            </li>
+          </div>
+          <div className="item">
+            <li>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFBVgrCZUwUT9V-rLSpQPj10C8reI2lUodOA&usqp=CAU"
+                alt=""
+              />
+              <h3>E-Commerce Backend</h3>
+            </li>
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
 }
