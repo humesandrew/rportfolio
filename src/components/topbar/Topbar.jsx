fad93e92a257c28a4ba262b211cda0d6637dbead
Andy Humes <andrew.humes@du.edu>
2022-04-10 13:54:39 -0600

    bootstrap topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,36 +1,29 @@
 import "./topbar.scss";
-import AccessibilityNewIcon from "@mui/icons-material/AccessibilityNew";
+import { Person, Mail } from "@material-ui/icons";
 
-export default function Topbar({
-  menuOpen, setMenuOpen
-}) {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            My logo
+            Andy Humes
           </a>
           <div className="itemContainer">
-            <AccessibilityNewIcon className="icon" />
-            <span>614-YOUR-MOM</span>
+            <Person className="icon" />
+            <span>Denver, CO</span>
           </div>
-
           <div className="itemContainer">
-            <AccessibilityNewIcon className="icon" />
-            <span>yourmom@yourmom.com</span>
+            <Mail className="icon" />
+            <span>humes.andrew@gmail.com</span>
           </div>
         </div>
-
         <div className="right">
-            <div className="hamburger">
-                <span className="line1"></span>
-                <span className="line2"></span>
-                <span className="line3"></span>
-            </div>
-
-
-
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>
     </div>
