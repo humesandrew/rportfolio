c46989b86cf63f22668c1178bcf50ea7d3e14588
Andy Humes <andrew.humes@du.edu>
2022-07-15 18:33:42 -0600

    added3projectcards

--- src/components/portfolioCard/PortfolioCard.jsx
+++ src/components/portfolioCard/PortfolioCard.jsx
@@ -9,7 +9,10 @@ import ProjectCard from "../projectCard/ProjectCard";
   export default function PortfolioCard() {
         return (
           <div class="projectContainer">
-            <div class="leftProject">Left
+            <div class="leftProject">
+              <h1>Featured</h1>
+          < ProjectCard />
+          < ProjectCard />
           < ProjectCard />
             </div>
             <div class="rightProject">Right</div>
