f5427630868841ed92c66eb4a4e030b3d7951618
Andy Humes <andrew.humes@du.edu>
2022-08-04 13:11:24 -0600

    addapicardskeleton

--- src/components/apiCard/ApiCard.jsx
+++ src/components/apiCard/ApiCard.jsx
@@ -7,37 +7,7 @@ export default function ApiCard() {
     <div>
 
       <ul>
-        {resumeData.featured &&
-          resumeData.featured.map((item) => {
-            return (
-              <div class="project">
-                <div class="projectDiv" style={{ backgroundImage: `url(${item.image})`}}>
-          {/* <div><img src={item.image}></img></div> */}
-          
-                  <h1> {item.name}</h1>
-
-                  <h4>{item.description}</h4>
-
-                  <h4>{item.technologies}</h4>
-
-                  <div class="linkDiv">
-                    <button>
-                      <a href={item.url} target="_blank" rel="noreferrer">
-                        {/* <h3>Deployed</h3> */}
-                        <h3>Deployed</h3>
-                      </a>
-                    </button>
-
-                    <button>
-                      <a href={item.url2} target="_blank" rel="noreferrer">
-                        <h3>Repository</h3>
-                      </a>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+   Hello World
       </ul>
     </div>
   );
