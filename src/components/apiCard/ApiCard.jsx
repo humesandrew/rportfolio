dac77299094bce6defbdd043c3514cc1cf9d7dc5
Andy Humes <andrew.humes@du.edu>
2022-08-04 13:35:35 -0600

    initialapicall

--- src/components/apiCard/ApiCard.jsx
+++ src/components/apiCard/ApiCard.jsx
@@ -1,14 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./apicard.scss";
 import resumeData from "../../resumeData.jsx";
+import axios from "axios";
 
 export default function ApiCard() {
-  return (
-    <div>
+  const [data, setData] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-      <ul>
-   Hello World
-      </ul>
-    </div>
+  useEffect(() => {
+    handlePosts();
+  }, []);
+
+  const handlePosts = async () => {
+    setLoading(true);
+    try {
+      const result = await axios.get(
+        "https://api.github.com/users/humesandrew/repos"
+      );
+      console.log(result);
+      setData(result.data);
+    } catch (err) {
+      setError(err.message || "Unexpected Error!");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+
+  return (
+    <ul>
+      {data.map(item => (
+        <li key={item.id}>
+          <a href={item.url}>{item.name}</a>
+        </li>
+      ))}
+    </ul>
   );
+
+
+
+
+  // return (
+  //   <div className="apiCard">
+  //     <div>
+  //       <h1>Posts</h1>
+  //       {loading && <p>Posts are loading!</p>}
+  //       {error && <p>{error}</p>}
+  //       <ul>
+  //         {posts.map((post) => (
+  //           <li key={post.id}>{post.title}</li>
+  //         ))}
+  //       </ul>
+  //     </div>
+  //   </div>
+  // );
 }
